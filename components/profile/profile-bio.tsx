21cceaecd5426f244613f998d82cf938c324ebc4
Hasanboy <hasanboyturdijonov@gmail.com>
2024-11-18 20:19:37 +0500

    Edit Modal

--- components/profile/profile-bio.tsx
+++ components/profile/profile-bio.tsx
@@ -8,10 +8,13 @@ import { BiCalendar } from "react-icons/bi";
 import { formatDistanceToNowStrict } from "date-fns";
 import axios from "axios";
 import { useRouter } from "next/navigation";
+import EditModal from "../modals/edit-modal";
+import useEditModal from "@/hooks/useEditModal";
 
 const ProfileBio = ({ user, userId }: { user: IUser; userId: string }) => {
   const [isLoading, setIsLoading] = useState(false);
 
+  const editModal = useEditModal();
   const router = useRouter();
 
   const onFollow = async () => {
@@ -48,76 +51,80 @@ const ProfileBio = ({ user, userId }: { user: IUser; userId: string }) => {
   const openFollowModal = async () => {};
 
   return (
-    <div className="border-b-[1px] border-neutral-800 pb-4">
-      <div className="flex justify-end p-2">
-        {userId === user._id ? (
-          <Button label="Edit Profile" secondary />
-        ) : (
-          <>
-            {user.isFollowing ? (
-              <Button
-                label={"Unfollow"}
-                disabled={isLoading}
-                onClick={onUnfollow}
-                outline
-              />
-            ) : (
-              <Button
-                label={"Follow"}
-                disabled={isLoading}
-                onClick={onFollow}
-              />
-            )}
-          </>
-        )}
-      </div>
-
-      <div className="mt-8 px-4">
-        <div className="flex flex-col">
-          <p className="text-white text-2xl font-semibold">{user.name}</p>
+    <>
+      <EditModal user={user} />
+      <div className="border-b-[1px] border-neutral-800 pb-4">
+        <div className="flex justify-end p-2">
+          {userId === user._id ? (
+            <Button label="Edit Profile" secondary onClick={editModal.onOpen} />
+          ) : (
+            <>
+              {user.isFollowing ? (
+                <Button
+                  label={"Unfollow"}
+                  disabled={isLoading}
+                  onClick={onUnfollow}
+                  outline
+                />
+              ) : (
+                <Button
+                  label={"Follow"}
+                  disabled={isLoading}
+                  onClick={onFollow}
+                />
+              )}
+            </>
+          )}
         </div>
 
-        <p className="text-md text-neutral-500">
-          {user.username ? `@${user.username}` : user.email}
-        </p>
+        <div className="mt-8 px-4">
+          <div className="flex flex-col">
+            <p className="text-white text-2xl font-semibold">{user.name}</p>
+          </div>
 
-        <div className="flex flex-col mt-4 ">
-          <p className="text-white">{user.bio}</p>
-          <div className="flex gap-4 items-center ">
-            {user.location && (
-              <div className="flex flex-row items-center gap-2 mt-4 text-sky-500 ">
-                <IoLocationSharp size={24} />
-                <p>{user.location}</p>
+          <p className="text-md text-neutral-500">
+            {user.username ? `@${user.username}` : user.email}
+          </p>
+
+          <div className="flex flex-col mt-4 ">
+            <p className="text-white">{user.bio}</p>
+            <div className="flex gap-4 items-center ">
+              {user.location && (
+                <div className="flex flex-row items-center gap-2 mt-4 text-sky-500 ">
+                  <IoLocationSharp size={24} />
+                  <p>{user.location}</p>
+                </div>
+              )}
+
+              <div className="flex flex-row items-center gap-2 mt-4 text-neutral-500 ">
+                <BiCalendar size={24} />
+                <p>
+                  Joined {formatDistanceToNowStrict(new Date(user.createdAt))}{" "}
+                  ago
+                </p>
               </div>
-            )}
-
-            <div className="flex flex-row items-center gap-2 mt-4 text-neutral-500 ">
-              <BiCalendar size={24} />
-              <p>
-                Joined {formatDistanceToNowStrict(new Date(user.createdAt))} ago
-              </p>
             </div>
-          </div>
-          <div className="flex flex-row items-center mt-6 gap-6">
-            <div
-              className="flex flex-row items-center gap-1 hover:underline cursor-pointer"
-              onClick={openFollowModal}
-            >
-              <p className="text-white">{user.following}</p>
-              <p className="text-neutral-500">Following</p>
-            </div>
+            <div className="flex flex-row items-center mt-6 gap-6">
+              <div
+                className="flex flex-row items-center gap-1 hover:underline cursor-pointer"
+                onClick={openFollowModal}
+              >
+                <p className="text-white">{user.following}</p>
+                <p className="text-neutral-500">Following</p>
+              </div>
 
-            <div
-              className="flex flex-row items-center gap-1 hover:underline cursor-pointer"
-              onClick={openFollowModal}
-            >
-              <p className="text-white">{user.followers}</p>
-              <p className="text-neutral-500">Followers</p>
+              <div
+                className="flex flex-row items-center gap-1 hover:underline cursor-pointer"
+                onClick={openFollowModal}
+              >
+                <p className="text-white">{user.followers}</p>
+                <p className="text-neutral-500">Followers</p>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
